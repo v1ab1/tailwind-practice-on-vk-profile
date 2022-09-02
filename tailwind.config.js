55e50192c55709c7f8d761e55220ed5c569bcf7b
v1ab1 <sharkandmouse@gmail.com>
2022-09-02 17:50:24 +0500

    begin

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'darkgray': '#141414'
+      },
+    },
+  }
